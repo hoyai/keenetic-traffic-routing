6225fa4d81a2468857e48ca24877a3070cdb57a8
hoyai <pikanenth@icloud.com>
2023-03-01 16:40:37 +0000

    AS15169 - full gooogle trafic vpn (youtube images)

--- rules/Google.js
+++ rules/Google.js
@@ -1,4 +1,5 @@
 /**
  * Google DNS to bypass tracking of visited sites by the provider when requesting the ip address of the site.
+ * AS15169 - full gooogle trafic vpn (youtube images)
  */
-module.exports = ["8.8.8.8", "8.8.4.4"];
+module.exports = ["8.8.8.8", "8.8.4.4", "AS15169"];
